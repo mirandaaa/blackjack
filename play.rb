ee538cc75a2e7d443d51290e9ecbef24df893b85
Miranda Wang <miranda.wang@MIRANDAWANG.local>
2016-07-08 10:27:25 -0400

    Adding win and end game logic, showing an X for the unknown dealer card

--- play.rb
+++ play.rb
@@ -3,6 +3,23 @@ require 'deck'
 require 'dealer'
 require 'player'
 
+def check_win(dealer, player)
+  if dealer.total == player.total
+    puts "TIE GAME"
+  end
+
+  if (21-dealer.total) < (21-player.total)
+    puts "DEALER WINS! Dealer total was #{dealer.total}, Player total was #{player.total}"
+  else
+    puts "PLAYER WINS! Player total was #{player.total}, Dealer total was #{dealer.total}"
+  end
+end
+
+def end_game
+  puts "Thanks for playing!"
+  exit!
+end
+
 deck = Deck.new
 
 puts "Welcome to Blackjack!"
@@ -18,27 +35,24 @@ puts "Cards in your hand: "
 puts p1.hand.each{ |card| card.to_s }
 puts "Your total is: #{p1.total}"
 
-puts "The dealer has: "
+puts "The dealer has: \nX"
 puts dealer.hand.drop(1).each{ |card| card.to_s }
 
 user_response = ""
 
-#Counting cards
+#Counting cards, 4 have already been dealt in the beginning of the game (2 to user, 2 to dealer)
 i = 3
 
-while user_response.casecmp("stand") == -1
+while user_response.casecmp("stand") == -1 || dealer.move?
   puts "Hit or stand?"
   user_response = gets.chomp
+
   if user_response.casecmp("hit") == 0
     puts "You hit!"
     p1.hand << deck.game_deck[i]
     p1.total = p1.calculate_total
-
-    #Check bust
-    #Dealer move
   elsif user_response.casecmp("stand") == 0
     puts "You stand!"
-    #Dealer move
   else
     puts "You did not type hit or stand! Please enter if you would like to hit or stand."
     redo
@@ -51,12 +65,28 @@ while user_response.casecmp("stand") == -1
 
   if p1.total > 21
     puts "YOU BUST!!!"
-    break
+    puts "Dealer wins."
+    end_game
   end
 
-  puts "The dealer has: "
-  dealer.hand << deck.game_deck[i+2]
+  if dealer.move?
+    puts "DEALER HITS"
+    dealer.hand << deck.game_deck[i+2]
+  else
+    puts "DEALER STANDS"
+  end
+
+  puts "The dealer has: \nX"
   puts dealer.hand.drop(1).each{ |card| card.to_s }
+  dealer.total = dealer.calculate_total
+  if dealer.total > 21
+    puts "DEALER BUSTS!!!"
+    puts "Player wins."
+    end_game
+  end
 
   i += 1
 end
+
+check_win(dealer, p1)
+end_game
--- player.rb
+++ player.rb
@@ -30,5 +30,4 @@ class Player
       }
     end
   end
-
 end
