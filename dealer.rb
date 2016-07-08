e4b30dc30a2add73e90c8a34b1c19329b1bcaa2d
Miranda Wang <miranda.wang@MIRANDAWANG.local>
2016-07-08 15:19:44 -0400

    Changing rule to dealer must hit when at 17, adding comment to explain dealer rule

--- dealer.rb
+++ dealer.rb
@@ -2,8 +2,9 @@ $LOAD_PATH << '.'
 require 'player'
 
 class Dealer < Player
+  #Dealer has extra rules - must hit until 17
   def move?
-    if self.total < 17
+    if self.total <= 17
       true
     else
       false
