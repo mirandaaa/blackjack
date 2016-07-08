9d0e1a4f64a569632c305400ac9deb1e7393964d
Miranda Wang <miranda.wang@MIRANDAWANG.local>
2016-07-08 15:19:28 -0400

    Changing logic to correct turn order in blackjack

--- play.rb
+++ play.rb
@@ -3,12 +3,10 @@ require 'deck'
 require 'dealer'
 require 'player'
 
-def check_win(dealer, player)
+def check_win(player, dealer)
   if dealer.total == player.total
-    puts "TIE GAME"
-  end
-
-  if (21-dealer.total) < (21-player.total)
+    puts "TIE GAME!"
+  elsif (21-dealer.total) < (21-player.total)
     puts "DEALER WINS! Dealer total was #{dealer.total}, Player total was #{player.total}"
   else
     puts "PLAYER WINS! Player total was #{player.total}, Dealer total was #{dealer.total}"
@@ -20,9 +18,24 @@ def end_game
   exit!
 end
 
-deck = Deck.new
+def play_hand
+end
+
+def check_bust(player, name)
+  if player.total > 21
+    puts "#{name} BUST!!!"
+    if name == "Dealer"
+      puts "Player wins!"
+    else
+      puts "Dealer wins!"
+    end
+    end_game
+  end
+end
 
 puts "Welcome to Blackjack!"
+deck = Deck.new
+
 p1 = Player.new
 p1.hand = [deck.game_deck[0], deck.game_deck[1]]
 p1.total = p1.calculate_total
@@ -39,11 +52,10 @@ puts "The dealer has: \nX"
 puts dealer.hand.drop(1).each{ |card| card.to_s }
 
 user_response = ""
-
-#Counting cards, 4 have already been dealt in the beginning of the game (2 to user, 2 to dealer)
 i = 3
 
-while user_response.casecmp("stand") == -1 || dealer.move?
+#Continue asking until they stand
+while user_response.casecmp("stand") == -1
   puts "Hit or stand?"
   user_response = gets.chomp
 
@@ -51,6 +63,7 @@ while user_response.casecmp("stand") == -1 || dealer.move?
     puts "You hit!"
     p1.hand << deck.game_deck[i]
     p1.total = p1.calculate_total
+    i += 1
   elsif user_response.casecmp("stand") == 0
     puts "You stand!"
   else
@@ -60,33 +73,75 @@ while user_response.casecmp("stand") == -1 || dealer.move?
 
   puts "Cards in your hand: "
   puts p1.hand.each{ |card| card.to_s }
-  p1.total = p1.calculate_total
   puts "Your total is: #{p1.total}"
-
-  if p1.total > 21
-    puts "YOU BUST!!!"
-    puts "Dealer wins."
-    end_game
-  end
-
-  if dealer.move?
-    puts "DEALER HITS"
-    dealer.hand << deck.game_deck[i+2]
-  else
-    puts "DEALER STANDS"
-  end
-
-  puts "The dealer has: \nX"
-  puts dealer.hand.drop(1).each{ |card| card.to_s }
-  dealer.total = dealer.calculate_total
-  if dealer.total > 21
-    puts "DEALER BUSTS!!!"
-    puts "Player wins."
-    end_game
-  end
-
-  i += 1
+  check_bust(p1, "Player")
 end
 
-check_win(dealer, p1)
+#After player stands, dealer turn!
+puts "The dealer has: "
+puts dealer.hand.each{ |card| card.to_s }
+
+while dealer.move?
+  puts "Dealer hits!"
+  dealer.hand << deck.game_deck[i+2]
+  dealer.total = dealer.calculate_total
+  i += 1
+
+  puts "The dealer has: "
+  puts dealer.hand.each{ |card| card.to_s }
+
+  check_bust(dealer, "Dealer")
+end
+
+puts "Dealer stands!"
+
+check_win(p1, dealer)
 end_game
+
+#
+# #Counting cards, 4 have already been dealt in the beginning of the game (2 to user, 2 to dealer)
+# i = 3
+#
+# while user_response.casecmp("stand") == -1
+#   puts "Hit or stand?"
+#   user_response = gets.chomp
+#
+#   if user_response.casecmp("hit") == 0
+#     puts "You hit!"
+#     p1.hand << deck.game_deck[i]
+#     p1.total = p1.calculate_total
+#   elsif user_response.casecmp("stand") == 0
+#     puts "You stand!"
+#   else
+#     puts "You did not type hit or stand! Please enter if you would like to hit or stand."
+#     redo
+#   end
+#
+#   puts "Cards in your hand: "
+#   puts p1.hand.each{ |card| card.to_s }
+#   p1.total = p1.calculate_total
+#   puts "Your total is: #{p1.total}"
+#
+#   if p1.total > 21
+#     puts "YOU BUST!!!"
+#     end_game
+#   end
+#
+#   if dealer.move?
+#     puts "DEALER HITS"
+#     dealer.hand << deck.game_deck[i+2]
+#   else
+#     puts "DEALER STANDS"
+#   end
+#
+#   puts "The dealer has: \nX"
+#   puts dealer.hand.drop(1).each{ |card| card.to_s }
+#   dealer.total = dealer.calculate_total
+#   if dealer.total > 21
+#     puts "DEALER BUSTS!!!"
+#     puts "Player wins."
+#     end_game
+#   end
+#
+#   i += 1
+# end
