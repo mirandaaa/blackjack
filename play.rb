9a2eab42f190ab7a373be070c6bcb65398e64f41
Miranda Wang <miranda.wang@MIRANDAWANG.local>
2016-07-12 10:21:55 -0400

    Reducing conditionals in check_win method and making totals print out shorter and more consistent

--- play.rb
+++ play.rb
@@ -3,28 +3,27 @@ require 'deck'
 require 'dealer'
 require 'player'
 
-def check_bust(player, name)
+def check_bust(player, dealer)
   if player.total > 21
-    puts "#{name} BUST!!!"
-    if name == 'Dealer'
-      puts 'Player wins!'
-    else
-      puts 'Dealer wins!'
-    end
+    puts 'PLAYER BUST!! Dealer wins! '\
+    "Player total: #{player.total}, Dealer total: #{dealer.total}"
+    end_game
+  end
+  if dealer.total > 21
+    puts 'DEALER BUST!! Player wins! '\
+    "Player total: #{player.total}, Dealer total: #{dealer.total}"
     end_game
   end
 end
 
 def check_win(player, dealer)
-  if dealer.total == player.total
-    puts 'TIE GAME!'
-  elsif (21 - dealer.total) < (21 - player.total)
-    puts 'DEALER WINS! '\
-    "Dealer total was #{dealer.total}, Player total was #{player.total}"
-  else
-    puts 'PLAYER WINS! '\
-    "Player total was #{player.total}, Dealer total was #{dealer.total}"
-  end
+  d = 21 - dealer.total # How close dealer total is to 21
+  pl = 21 - player.total # How close player total is to 21
+  puts 'TIE GAME!' if d == pl
+  puts 'DEALER WINS! '\
+  "Player total: #{player.total}, Dealer total: #{dealer.total}" if d < pl
+  puts 'PLAYER WINS! '\
+  "Player total: #{player.total}, Dealer total: #{dealer.total}" if d > pl
 end
 
 def end_game
@@ -54,7 +53,7 @@ puts dealer.hand.drop(1).each(&:to_s)
 user_response = ''
 
 # Keeping track of cards in the deck, 4 have already been dealt
-card_num = 3
+card_num = 4
 
 # Continue asking until player stands
 until user_response.casecmp('stand') == 0
@@ -69,7 +68,7 @@ until user_response.casecmp('stand') == 0
     puts 'Cards in your hand: '
     puts p1.hand.each(&:to_s)
     puts "Your total is: #{p1.total}"
-    check_bust(p1, 'Player')
+    check_bust(p1, dealer)
   elsif user_response.casecmp('stand') == 0
     puts 'You stand!'
   else
@@ -93,7 +92,7 @@ while dealer.move?
   puts dealer.hand.each(&:to_s)
 
   dealer.total = dealer.calculate_total
-  check_bust(dealer, 'Dealer')
+  check_bust(p1, dealer)
 end
 
 puts 'Dealer stands!'
