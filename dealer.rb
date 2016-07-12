a17e9011c4ea07721c49c31c56c641951626b1bc
Miranda Wang <miranda.wang@MIRANDAWANG.local>
2016-07-12 10:22:11 -0400

    Small comment change

--- dealer.rb
+++ dealer.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH << '.'
 require 'player'
 
-# Class for dealer, a type of player, with extra rules - must hit until 17
+# Class for dealer, a type of player with extra rules - must hit until 17
 class Dealer < Player
   def move?
     if total <= 17
