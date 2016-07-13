9e72b19dba9170aaf444bb27dab89e982eb082cd
Miranda Wang <miranda.wang@MIRANDAWANG.local>
2016-07-13 16:42:49 -0400

    Moving get_card_val method to the Card class to encapsulate its logic from Player

--- card.rb
+++ card.rb
@@ -1,11 +1,24 @@
 # Class structure for card, it has a numeric value and a suit
 class Card
-  attr_reader :val, :suit
   attr_accessor :val, :suit
 
   def initialize(val, suit)
     @val = val
     @suit = suit
+  end
+
+  def get_card_val(sum)
+    # Face cards count as 10
+    if %w(J K Q).include?(@val)
+      10
+    # Ace should count as 1 or 11
+    elsif @val == 'A' && sum + 11 >= 21
+      1
+    elsif @val == 'A' && sum + 11 < 21
+      11
+    else
+      @val.to_i
+    end
   end
 
   def to_s
--- player.rb
+++ player.rb
@@ -8,23 +8,9 @@ class Player
     @total = calculate_total
   end
 
-  def get_card_val(sum, card)
-    # Face cards count as 10
-    if %w(J K Q).include?(card.val)
-      10
-    # Ace should count as 1 or 11
-    elsif card.val == 'A' && sum + 11 >= 21
-      1
-    elsif card.val == 'A' && sum + 11 < 21
-      11
-    else
-      card.val.to_i
-    end
-  end
-
   def calculate_total
     hand.inject(0) do |sum, card|
-      sum + get_card_val(sum, card)
+      sum + card.get_card_val(sum)
     end
   end
 end
