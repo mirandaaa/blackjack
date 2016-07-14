7c59d708bc47655a2bd8e742878def7b5ac815ff
Miranda Wang <miranda.wang@MIRANDAWANG.local>
2016-07-14 13:17:31 -0400

    Separating logic out into methods and including a  check to make sure this file can only be run through the command line

--- play.rb
+++ play.rb
@@ -2,17 +2,8 @@ require_relative 'deck'
 require_relative 'dealer'
 require_relative 'player'
 
-def check_bust(player, dealer)
-  if player.total > 21
-    puts 'PLAYER BUST!! Dealer wins! '\
-    "Player total: #{player.total}, Dealer total: #{dealer.total}"
-    end_game
-  end
-  if dealer.total > 21
-    puts 'DEALER BUST!! Player wins! '\
-    "Player total: #{player.total}, Dealer total: #{dealer.total}"
-    end_game
-  end
+def check_bust(player)
+  player.busted?
 end
 
 def check_win(player, dealer)
@@ -30,67 +21,71 @@ def end_game
   exit!
 end
 
-# Game starts here...
-puts 'Welcome to Blackjack!'
-deck = Deck.new
+def play_player_hand(p1, deck)
+  user_response = ''
+  # Continue asking until player stands
+  until user_response.casecmp('stand') == 0
+    puts 'Hit or stand?'
+    user_response = gets.chomp
 
-p1 = Player.new
-p1.hand = [deck.draw!, deck.draw!]
-p1.total
-
-dealer = Dealer.new
-dealer.hand = [deck.draw!, deck.draw!]
-dealer.total
-
-puts 'Cards in your hand: '
-puts p1.hand.each(&:to_s)
-puts "Your total is: #{p1.total}"
-
-puts "The dealer has: \nX"
-puts dealer.hand.drop(1).each(&:to_s)
-
-user_response = ''
-
-# Keeping track of cards in the deck, 4 have already been dealt
-card_num = 4
-
-# Continue asking until player stands
-until user_response.casecmp('stand') == 0
-  puts 'Hit or stand?'
-  user_response = gets.chomp
-
-  if user_response.casecmp('hit') == 0
-    puts 'You hit!'
-    p1.hit!(deck.draw!)
-    card_num += 1
-    puts 'Cards in your hand: '
-    puts p1.hand.each(&:to_s)
-    puts "Your total is: #{p1.total}"
-    check_bust(p1, dealer)
-  elsif user_response.casecmp('stand') == 0
-    puts 'You stand!'
-  else
-    puts 'You did not type hit or stand!'\
-    'Please enter if you would like to hit or stand.'
-    redo
+    if user_response.casecmp('hit') == 0
+      puts 'You hit!'
+      p1.hit!(deck.draw!)
+      puts 'Cards in your hand: '
+      puts p1.hand.each(&:to_s)
+      puts "Your total is: #{p1.total}"
+      if check_bust(p1)
+        puts 'PLAYER BUST!!! Dealer wins!'
+        end_game
+      end
+    elsif user_response.casecmp('stand') == 0
+      puts 'You stand!'
+    else
+      puts 'You did not type hit or stand!'\
+      'Please enter if you would like to hit or stand.'
+      redo
+    end
   end
 end
 
-# After player stands, dealer turn!
-# Reveal the "X" card
-puts 'The dealer reveals hand: '
-puts dealer.hand.each(&:to_s)
-
-while dealer.move?
-  puts 'Dealer hits!'
-  dealer.hit!(deck.draw!)
-  card_num += 1
-
-  puts 'The dealer has: '
+def play_dealer_hand(dealer, deck)
+  # After player stands, dealer turn!
+  # Reveal the "X" card
+  puts 'The dealer reveals hand: '
   puts dealer.hand.each(&:to_s)
-  check_bust(p1, dealer)
+
+  while dealer.move?
+    puts 'Dealer hits!'
+    dealer.hit!(deck.draw!)
+    puts 'The dealer has: '
+    puts dealer.hand.each(&:to_s)
+    if check_bust(dealer)
+      puts 'DEALER BUST!!! Player wins!'
+      end_game
+    end
+  end
+  puts 'Dealer stands!'
 end
 
-puts 'Dealer stands!'
-check_win(p1, dealer)
-end_game
+def play_game(player, dealer, deck)
+  # Game starts here...
+  puts 'Welcome to Blackjack!'
+  puts 'Cards in your hand: '
+  puts player.hand.each(&:to_s)
+  puts "Your total is: #{player.total}"
+  puts "The dealer has: \nX"
+  puts dealer.hand.drop(1).each(&:to_s)
+  play_player_hand(player, deck)
+  play_dealer_hand(dealer, deck)
+  check_win(player, dealer)
+end
+
+if __FILE__ == $0
+  deck = Deck.new
+  p1 = Player.new
+  p1.hand = [deck.draw!, deck.draw!]
+  dealer = Dealer.new
+  dealer.hand = [deck.draw!, deck.draw!]
+  play_game(p1, dealer, deck)
+  end_game
+end
